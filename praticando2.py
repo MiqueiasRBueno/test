bfbddd6a3a62d15374a14b1beb36573bf683e605
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-08 23:51:54 -0300

    create - praticando2.py

--- praticando2.py
+++ praticando2.py
@@ -1,9 +1,15 @@
 from random import randint
 from time import sleep
 
+print(f'''\033[1;31m{'=' * 55}\033[m
+{'Jogo de Dados':^55}
+\033[1;31m{'=' * 55}\033[m''')
 jogadores = dict()
 for j in range(0, 4):
     jogadores[f'jogador {j + 1}'] = randint(1, 6)
+print(f'''{'Valores Sorteados':^55}
+\033[1;33m{'-' * 55}\033[m''')
 for k, v in jogadores.items():
-    sleep(0.8)
+    sleep(1)
     print(f'{f'{k}'.title()} tirou {v}')
+print(f'\033[1;33m{'-' * 55}\033[m')
